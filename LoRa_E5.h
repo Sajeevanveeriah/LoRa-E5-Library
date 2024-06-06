ed2f28089b0efdcb631a22957cdf974684ec984f
Sajeevan Veeriah <sajeevanveeriah@gmail.com>
2024-06-06 19:35:03 +1000

    Update LoRa_E5.h

--- LoRa_E5.h
+++ LoRa_E5.h
@@ -7,8 +7,8 @@ class LoRa_E5 {
 public:
     LoRa_E5(HardwareSerial &serial, uint32_t baud, int8_t rxPin, int8_t txPin);
     void begin();
-    bool sendATCommand(const String& command);
-    String readResponse(uint16_t timeout = 1000);
+    bool sendATCommand(const String& command, String& response);
+    String readResponse(uint16_t timeout = 2000); // Increased default timeout
     bool initializeModule();
     bool joinNetwork(const String& devEUI, const String& appEUI, const String& appKey);
     bool sendMessage(const String& data);
